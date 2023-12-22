c73406cfb1669b27b92a42839a5cecec0c4d3da1
sibi <sibisk78@gmail.com>
2023-12-22 13:31:15 +0530

    terraform 2 st server

--- sample.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "centos" {
-  most_recent = true
-  owners      = ["973714476881"]
-  name_regex = "Centos-8-DevOps-Practice"
-}
-
-output "centos_ami_id" {
-  value = data.aws_ami.centos.id
-}

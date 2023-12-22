dffb95be9b7394beddd4f8cf052a24cc82573354
sibi <sibisk78@gmail.com>
2023-12-22 12:39:35 +0530

    terraform 2 st server

--- sample.tf
+++ sample.tf
@@ -1,7 +1,7 @@
-data "aws_security_group" "workstation" {
+data "aws_security_group" "default" {
   name = "allow all"
 }
 
 output "security_groups" {
-  value = data.aws_security_group.workstation
+  value = data.aws_security_group.default
 }

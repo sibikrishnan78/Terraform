09953189e0d5db74b17e48da57dabd0b8a4d21ff
sibi <sibisk78@gmail.com>
2023-12-25 20:10:00 +0530

    for each

--- server.tf
+++ server.tf
@@ -12,200 +12,72 @@ variable "instance_type" {
   default = "t3.micro"
 }
 
-resource "aws_instance" "frontend" {
+variable "component" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.small"
+    }
+    mongodb = {
+      name = "mongodb"
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.small"
+    }
+    redis = {
+      name = "redis"
+      instance_type = "t3.small"
+    }
+    user = {
+      name = "user"
+      instance_type = "t3.small"
+    }
+    cart = {
+      name = "cart"
+      instance_type = "t3.small"
+    }
+    mysql = {
+      name = "mysql"
+      instance_type = "t3.small"
+    }
+    shipping = {
+      name = "shipping"
+      instance_type = "t3.small"
+    }
+    rabbitmq = {
+      name = "rabbitmq"
+      instance_type = "t3.small"
+    }
+    payment = {
+      name = "payment"
+      instance_type = "t3.small"
+    }
+    dispatch = {
+      name = "dispatch"
+      instance_type = "t3.small"
+    }
+
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each = var.component
   ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
+  instance_type = each.value["instance_type"]
   vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
 
   tags = {
-    Name = "frontend"
+    Name = each.value["name"]
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "frontend.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "mongodb.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "catalogue.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-
-resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_route53_record" "redis" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "redis.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-resource "aws_instance" "user" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [  data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_route53_record" "user" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "user.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-
-resource "aws_instance" "cart" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [  data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_route53_record" "cart" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "cart.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "mysql.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-resource "aws_instance" "shipping" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "shipping.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "rabbitmq.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-resource "aws_instance" "payment" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_route53_record" "payment" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "payment.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
-
-resource "aws_instance" "dispatch" {
-  ami           = data.aws_ami.centos.id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.all-security.id ]
-
-  tags = {
-    Name = "dispatch1"
-  }
-}
-
-resource "aws_route53_record" "dispatch" {
-  zone_id = "Z08619273P4WVGQRW042K"
-  name    = "dispatch.deveng23.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.dispatch.private_ip]
-}
+#resource "aws_route53_record" "frontend" {
+#  zone_id = "Z08619273P4WVGQRW042K"
+#  name    = "frontend.deveng23.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.frontend.private_ip]
+#}
+#

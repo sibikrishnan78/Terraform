45d398e364579202e8a2c0403bbbc6c2a0d2d06a
sibi <sibisk78@gmail.com>
2023-12-28 11:39:06 +0530

    attachment between server and code

--- server.tf
+++ server.tf
@@ -9,27 +9,27 @@ resource "aws_instance" "instance" {
   }
 }
 
-#resource "null_resource" "provisioner" {
-#
-#  depends_on = [aws_instance.instance, aws_route53_record.records]
-#  for_each = var.component
-#  provisioner "remote-exec" {
-#
-#    connection {
-#      type     = "ssh"
-#      user     = "centos"
-#      password = "DevOps321"
-#      host     = aws_instance.instance[each.value["name"]].private_ip
-#    }
-#
-#    inline = [
-#      "rm -rf Roboshop",
-#      "git clone https://github.com/sibikrishnan78/Roboshop.git",
-#      "cd Roboshop/",
-#      "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password", "null")}"
-#    ]
-#  }
-#}
+resource "null_resource" "provisioner" {
+
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  for_each = var.component
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance[each.value["name"]].private_ip
+    }
+
+    inline = [
+      "rm -rf Roboshop",
+      "git clone https://github.com/sibikrishnan78/Roboshop.git",
+      "cd Roboshop/",
+      "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password", "null")}"
+    ]
+  }
+}
 
 
 resource "aws_route53_record" "records" {
